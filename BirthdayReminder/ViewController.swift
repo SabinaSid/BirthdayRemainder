f2ad936223a6887d057577c02f429afbf9ddf25c
Sabina Grinenko <kaiser2169@gmail.com>
2023-07-11 18:20:18 +0400

    init

---
+++ BirthdayReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayReminder
+//
+//  Created by Sabina Grinenko on 11.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
