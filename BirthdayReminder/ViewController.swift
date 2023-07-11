0393d40f1744c5360ceb3145e98e290137f39005
Sabina Grinenko <kaiser2169@gmail.com>
2023-07-11 18:36:39 +0400

    save

--- BirthdayReminder/ViewController.swift
+++ BirthdayReminder/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.   asdasd
+        // Do any additional setup after loading the view.   aasdassdasd
     }
 
 
